92cdb09c0611f0bc99a353e93728eed24ca1365d
jeepee12 <jeepee.12@hotmail.com>
2018-08-22 12:31:01 -0400

    Adds a date range picker that allow you to see game times based on that range.

--- gamelaunchercloud-react/src/App.js
+++ gamelaunchercloud-react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
+import DatePicker from 'react-date-picker';
 
 // Based on : https://firebase.google.com/docs/database/web/start?authuser=0
 // Set the configuration for your app
@@ -21,9 +22,12 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      dateStart: new Date(),
+      dateEnd: new Date(),
       games: []
     };
-    this.CalculateGameTime = this.CalculateGameTime.bind(this);
+    this.CalculateAllTimeGameTime = this.CalculateAllTimeGameTime.bind(this);
+    this.CalculateRangeGameTime = this.CalculateRangeGameTime.bind(this);
   }
 
   componentDidMount() {
@@ -45,15 +49,30 @@ class App extends Component {
     });
   }
 
-  CalculateGameTime(gametimes) {
+  CalculateAllTimeGameTime(gametimes) {
     var totalMinutes = 0;
     for (let time in gametimes) {
-      console.log(gametimes[time]);
+      //console.log(gametimes[time]);
       totalMinutes += gametimes[time].Value.NbMinutes;
     }
 
     return totalMinutes;
   }
+
+  CalculateRangeGameTime(gametimes) {
+    var totalMinutes = 0;
+    for (let time in gametimes) {
+      var date = new Date(gametimes[time].Key);
+      if (date >= this.state.dateStart && date <= this.state.dateEnd) {
+        totalMinutes += gametimes[time].Value.NbMinutes;
+      }
+    }
+
+    return totalMinutes;
+  }
+
+  dateStartOnChange = dateStart => this.setState({ dateStart })
+  dateEndOnChange = dateEnd => this.setState({ dateEnd })
 
   render() {
     return (
@@ -62,6 +81,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Tritor Gamelauncher Dashboard!</h1>
         </header>
+
+        <section className='totalGameTime'>
+          <div>
+            Range starts at:
+        <DatePicker
+              onChange={this.dateStartOnChange}
+              value={this.state.dateStart}
+            />
+            Range ends at:
+        <DatePicker
+              onChange={this.dateEndOnChange}
+              value={this.state.dateEnd}
+            />
+          </div>
+        </section>
+
         <p className="App-intro">
           These games are tracked:
         </p>
@@ -72,8 +107,8 @@ class App extends Component {
                 return (
                   <li key={game.id}>
                     <h3>Game:{game.name}</h3>
-                    <p>Total time: {this.CalculateGameTime(game.gametimes)}</p>
-
+                    <p>Range total time: {this.CalculateRangeGameTime(game.gametimes)}</p>
+                    <p>All time total time: {this.CalculateAllTimeGameTime(game.gametimes)}</p>
                   </li>
                 )
               })}
